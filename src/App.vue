6ce7548dac5c857ec6f1785283c648b7f9392076
AlvaroLe6 <alvaro_147@hotmail.es>
2024-04-23 18:39:11 -0400

    23

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,85 @@
-<script setup>
-import Footer from "@/views/Footer.vue";
-import { RouterView } from "vue-router";
-import { useAuthStore } from "./stores/auth";
-import UserProfile from '@/layouts/UserProfile.vue'
+  <script setup>
+  import Footer from "@/views/Footer.vue";
+  import { RouterView } from "vue-router";
+  import { useAuthStore } from "./stores/auth";
+  import UserProfile from '@/layouts/UserProfile.vue'
 
 
-const auth = useAuthStore();
-</script>
-<template>
-  <v-app>
-    <v-card elevate="3" class="fill-height">
-      <v-app-bar app color="#162D4B" class="app-bar" >
-        <v-container class="d-flex align-center justify-space-between">
+  const auth = useAuthStore();
+  </script>
+  <template>
+    <v-app>
+        <v-app-bar app color="#162D4B" class="app-bar" >
+          <div class="nav-container">
+            <router-link :to="{ name: 'login' }"  class="logo-link">
+              <img
+                src="@/assets/ESCUELA DE NEGOCIOS_HORIZONTAL BLANCO.png"
+                alt="Logo"
+                height="80"
+              />
+            </router-link>
+        
+            <v-spacer></v-spacer>
 
-       
-          <router-link :to="{ name: 'login' }">
-            <img
-              src="@/assets/ESCUELA DE NEGOCIOS_HORIZONTAL BLANCO.png"
-              alt="80"
-              height="80"
-            />
-          </router-link>
-        <div class="app-bar-actions">
-          <div 
-          v-if="auth.isAuth" 
-          >
-            <v-btn :to="{ name: 'admin-list-contabilidad' }" 
-            class="button"> 
-            Admin 
-          </v-btn>      
-            <UserProfile />
-          </div>
-          <div v-else>
-            <v-btn :to="{ name: 'login' }"> Iniciar Sesión </v-btn>
+            <div class="right-section">          
+              <v-btn
+              v-if="auth.isAuth"
+              :to="{ name: 'admin-list-contabilidad' }" 
+              class="mr-4"> 
+              Admin 
+            </v-btn>
+                  
+              <UserProfile v-if="auth.isAuth" />
+        
+      
+              <v-btn 
+              v-else
+              :to="{ name: 'login' }"> Iniciar Sesión </v-btn>
+            
           </div>
         </div>
-       </v-container>
-      </v-app-bar>
-      <v-main style="margin-bottom: 50px">
-        <v-container  fluid="true">
-          <router-view />
-        </v-container>
-      </v-main>
-    </v-card>
-    <Footer />
-  </v-app>
-</template>
-<script>
-export default {
-  data: () => ({
-    items: [
-      { title: "Cursos", route: "cursos" },
-      { title: "Webinar", route: "webinar" },
-    ],
-  }),
-  methods: {
-    redirectToView(route) {
-      this.$router.push({ name: route });
+        </v-app-bar>
+        <v-main style="margin-bottom: 50px">
+          <v-container  fluid="true">
+            <router-view />
+          </v-container>
+        </v-main>
+      <Footer />
+    </v-app>
+  </template>
+  <script>
+  export default {
+    data: () => ({
+      items: [
+        { title: "Cursos", route: "cursos" },
+        { title: "Webinar", route: "webinar" },
+      ],
+    }),
+    methods: {
+      redirectToView(route) {
+        this.$router.push({ name: route });
+      },
     },
-  },
-};
-</script>
-<style>
-.app-bar-container {
-  display: flex; /* Activa flexbox */
-  align-items: center; /* Alinea los elementos verticalmente al centro */
-  justify-content: space-between; /* Espacio entre el logo y las acciones del lado derecho */
-  width: 100%; /* Asegura que el contenedor use todo el ancho disponible */
-}
+  };
+  </script>
+  <style scoped>
+  .nav-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
 
-.app-bar-actions {
-  display: flex; /* Activa flexbox para los elementos de la derecha */
-  align-items: center; /* Alinea los elementos verticalmente al centro */
-}
+  .logo-link {
+    margin-right: 1rem; /* Ajusta esto para el espacio después del logo */
+  }
 
-.button {
-  margin-right: 20px; /* Espacio a la derecha del botón Admin */
-}
-.container {
-    text-align: center;
-}
-
-.button {
-    margin-bottom: 10px;
-}
-.app-bar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 1rem; /* ajusta el relleno según sea necesario */
-}
-
-
-.user-actions {
-  display: flex;
-  align-items: center;
-  gap: 1rem; /* espacio entre el botón y UserProfile */
-}
-</style>
+  .right-section {
+    display: flex;
+    align-items: center;
+    gap: 1rem; /* Ajusta esto para el espacio entre los elementos */
+  }
+  .logo-link {
+    margin-right: 1rem; /* Ajusta esto para el espacio después del logo */
+  }
+  </style>
